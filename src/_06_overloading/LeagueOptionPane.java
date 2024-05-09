3ee8c4395ecd441568a956e076bf4ae313846e65
league <league@192.168.1.200>
2024-05-08 20:01:14 -0700

    neal fun

--- src/_06_overloading/LeagueOptionPane.java
+++ src/_06_overloading/LeagueOptionPane.java
@@ -20,10 +20,19 @@ public class LeagueOptionPane {
 	public static void showMessageDialog(String message) {
 		// 1. Open example.png and make a GUI that looks like that
 		//    The message parameter is what we want to show on our pop-up
-		
+		JFrame frame = new JFrame();
+		JPanel pan = new JPanel();
+		JLabel text = new JLabel();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		text.setText(message);
+		text.setIcon(loadImage("league.png"));
+		pan.add(text);
+		frame.add(pan);
+		frame.pack();
 		
 		// 2. Uncomment the line of code below. It sets the location of our frame to the center of the screen
-		//frame.setLocationRelativeTo(null);
+		frame.setLocationRelativeTo(null);
 	}
 	
 	// 3. Call this method in the Runner class
--- src/_06_overloading/LeagueOptionPaneRunner.java
+++ src/_06_overloading/LeagueOptionPaneRunner.java
@@ -1,7 +1,12 @@
 package _06_overloading;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 public class LeagueOptionPaneRunner {
 	public static void main(String[] args) {
-		
+		LeagueOptionPane pain = new LeagueOptionPane();
+		pain.showMessageDialog("lol");
 	}
 }
